d91febd7e404f89a96697abb25bac03f6f9d419f
Premek Vysoky <premek.vysoky@seznam.cz>
2016-03-15 11:30:34 +0100

    Parser - add unit tests

---
+++ parser/src/premun/mps/ingrid/parser/grammar/test/LiteralRuleTest.java
@@ -0,0 +1,20 @@
+package premun.mps.ingrid.parser.grammar.test;
+
+import org.junit.*;
+import premun.mps.ingrid.parser.grammar.*;
+
+import static org.junit.Assert.*;
+
+public class LiteralRuleTest {
+    @Test
+    public void testInitialization() {
+        LiteralRule rule = new LiteralRule("'<'");
+        assertEquals("<", rule.getContent());
+
+        rule = new LiteralRule(">");
+        assertEquals(">", rule.getContent());
+
+        rule = new LiteralRule("");
+        assertEquals("", rule.getContent());
+    }
+}
