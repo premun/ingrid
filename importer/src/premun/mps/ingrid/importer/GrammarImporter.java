bfdef2c467cd0433eff15359eb0d8e042e017d3d
Premek Vysoky <premek.vysoky@seznam.cz>
2016-03-18 10:13:57 +0100

    Importer - Add importAlternative() + comments

--- importer/src/premun/mps/ingrid/importer/GrammarImporter.java
+++ importer/src/premun/mps/ingrid/importer/GrammarImporter.java
@@ -8,6 +8,8 @@ import org.jetbrains.mps.openapi.model.*;
 import premun.mps.ingrid.parser.*;
 import premun.mps.ingrid.parser.grammar.*;
 
+import java.util.*;
+
 public class GrammarImporter {
     private SModel structureModel;
     private GrammarInfo grammar;
@@ -16,7 +18,11 @@ public class GrammarImporter {
         this.structureModel = structureModel;
     }
 
+    /**
+     * Prepares the target language for import (clears it away).
+     */
     private void initializeLanguage() {
+        // TODO: Check if language exists and create it if it doesn't (or delete it and create it every time)
         // Delete all nodes
         SModelOperations
             .nodes(this.structureModel, null)
@@ -24,29 +30,44 @@ public class GrammarImporter {
             .forEach(SNodeOperations::deleteNode);
     }
 
+    /**
+     * Main method of the import process.
+     * @param fileName Name of the ANTLR grammar file to be imported.
+     */
     public void importGrammar(String fileName) {
+        // TODO: should receive name of the language too
         initializeLanguage();
 
         this.grammar = GrammarParser.parseFile(fileName);
 
         this.importTokens();
         this.importRules();
+
+        // Set root rule's concept as rootable
+        SNode rootConcept = this.translateRuleToConcept(this.grammar.rootRule);
+        NodeHelper.setProperty(rootConcept, Properties.ROOTABLE, "true");
     }
 
+    /**
+     * Imports all regex lexer rules as constraint data type concepts.
+     */
+    private void importTokens() {
+        this.grammar.rules
+            .values()
+            .stream()
+            .filter(r -> r instanceof RegexRule)
+            .forEach(r -> this.importToken((RegexRule) r));
+    }
+
+    /**
+     * Import all parser rules as either interfaces (split rules) or actual concepts.
+     */
     private void importRules() {
         this.grammar.rules
             .values()
             .stream()
             .filter(r -> r instanceof ParserRule)
             .forEach(r -> this.importRule((ParserRule) r));
-    }
-
-    private void importTokens() {
-        this.grammar.rules
-            .values()
-            .stream()
-            .filter(r -> r instanceof FlatLexerRule)
-            .forEach(r -> this.importToken((FlatLexerRule) r));
     }
 
     private void importRule(ParserRule rule) {
@@ -57,33 +78,57 @@ public class GrammarImporter {
             this.structureModel.addRootNode(iface);
 
             // For each alternative, there will be a concept
-            for (int i = 1; i <= rule.alternatives.size(); ++i) {
+            for (int i = 0; i < rule.alternatives.size(); ++i) {
                 // TODO: if only one element is contained inside, we can flatten this rule and delete this intermediate step by advancing to the next step
-                String name = rule.name + "_" + i;
-                SNode alternativeNode = NodeHelper.createNode(NodeType.Concept, name, name, "Rules", this.structureModel);
-                this.structureModel.addRootNode(alternativeNode);
+                String name = rule.name + "_" + (i + 1);
+                SNode alternativeNode = this.importAlternative(rule.alternatives.get(i), name);
                 this.linkInterfaceToConcept(iface, alternativeNode);
             }
         } else {
-            // Concrete element, we can create a concept
-            SNode node = NodeHelper.createNode(NodeType.Concept, rule.name, rule.name, "Rules", this.structureModel);
-
-            if (rule.equals(this.grammar.rootRule)) {
-                NodeHelper.setProperty(node, Properties.ROOTABLE, "true");
-            }
-
-            this.structureModel.addRootNode(node);
+            this.importAlternative(rule.alternatives.get(0), rule.name);
         }
     }
 
-    private void importToken(FlatLexerRule rule) {
-        if (rule instanceof RegexRule) {
-            SNode node = NodeHelper.createNode(NodeType.ConstraintDataType, rule.name, rule.name, "Tokens", this.structureModel);
-            NodeHelper.setProperty(node, Properties.CONSTRAINT, ((RegexRule) rule).regexp);
-            this.structureModel.addRootNode(node);
-        }
+    private void importToken(RegexRule rule) {
+        SNode node = NodeHelper.createNode(NodeType.ConstraintDataType, rule.name, rule.name, "Tokens", this.structureModel);
+        NodeHelper.setProperty(node, Properties.CONSTRAINT, ((RegexRule) rule).regexp);
+        this.structureModel.addRootNode(node);
     }
 
+    /**
+     * Imports an alternative, which is a real concept containing several rules.
+     *
+     * Alternative is imported together with editor, that contains literal rules.
+     * All children rules are set as children
+     * Example:
+     * element   :   '<' Name '>' Content '</' Name '>'
+                 |   '<' Name '/>'
+                 ;
+     *
+     * There will be created two concepts for this rule (two alternatives).
+     *
+     * @param alternative List of elements of given alternative.
+     * @param name Name of the concept.
+     * @return Created concept's node.
+     */
+    private SNode importAlternative(List<RuleReference> alternative, String name) {
+        // Concrete element, we can create a concept
+        SNode node = NodeHelper.createNode(NodeType.Concept, name, name, "Rules", this.structureModel);
+
+        // TODO: add children elements (ParserRule/RegexRule children)
+        // TODO: create editor
+        // TODO: add literal rules into editor aspect
+
+        this.structureModel.addRootNode(node);
+
+        return node;
+    }
+
+    /**
+     * Links an interface node and a concept node together (adds the interface to "implements" field).
+     * @param iface Interface to be added.
+     * @param concept Target concept node.
+     */
     private void linkInterfaceToConcept(SNode iface, SNode concept) {
         // Create interface link
         SNode interfaceReference = NodeHelper.createNode(NodeType.InterfaceReference, null, null, null, null);
@@ -96,6 +141,11 @@ public class GrammarImporter {
         ListSequence.fromList(SLinkOperations.getChildren(concept, implementsLink)).addElement(interfaceReference);
     }
 
+    /**
+     * Finds a concept that was created for given rule.
+     * @param rule Rule to be matched.
+     * @return Concept node belonging to given rule.
+     */
     private SNode translateRuleToConcept(Rule rule) {
         for (SNode node : this.structureModel.getRootNodes()) {
             if (rule.name.equals(node.getName())) {
