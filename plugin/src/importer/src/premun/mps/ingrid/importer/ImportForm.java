9ea9ad9dafd6fef9e4ea755eb5d4337dcc470ddb
Premek Vysoky <premek.vysoky@seznam.cz>
2016-07-06 15:24:56 +0200

    Add the import form

---
+++ plugin/src/importer/src/premun/mps/ingrid/importer/ImportForm.java
@@ -0,0 +1,175 @@
+package premun.mps.ingrid.importer;
+
+import javax.swing.*;
+import javax.swing.filechooser.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+/**
+ * Class represents an input form, that prompts user for import data.
+ */
+public class ImportForm extends JFrame {
+    private JList fileList;
+    private JButton addFileButton;
+    private JButton removeFileButton;
+    private JPanel languagePanel;
+    private JPanel buttonPanel;
+    private JPanel fileButtonPanel;
+    private JPanel fileLabelPanel;
+    private JPanel fileListPanel;
+    private JPanel filePanel;
+    private JPanel mainPanel;
+    private JButton importButton;
+    private JButton cancelButton;
+    private JComboBox languages;
+    private JTextField languageName;
+    private JPanel newLanguagePanel;
+
+    public List<File> files;
+    private boolean confirmed;
+
+    public ImportForm(List<String> mpsLanguages) {
+        this.files = new ArrayList<>();
+        this.confirmed = false;
+
+        this.setTitle("Import ANTLRv4 grammar");
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setContentPane(this.mainPanel);
+        this.setSize(600, 400);
+        this.setVisible(true);
+
+        // Main buttons
+        this.cancelButton.addActionListener(e -> this.cancelDialog());
+        this.importButton.addActionListener(e -> this.confirmDialog());
+
+        // Plus / minus buttons
+        this.addFileButton.addActionListener(e -> this.addFiles());
+
+        this.removeFileButton.setEnabled(false);
+        this.removeFileButton.addActionListener(e -> this.removeFiles());
+
+        // File list
+        this.fileList.setModel(new DefaultListModel<String>());
+        this.fileList.addListSelectionListener(e ->
+            this.removeFileButton.setEnabled(!this.fileList.isSelectionEmpty())
+        );
+
+        // Language combo box
+        DefaultComboBoxModel<String> languageModel = new DefaultComboBoxModel<>();
+        mpsLanguages
+            .stream()
+            .forEach(languageModel::addElement);
+        languageModel.addElement("New language...");
+        this.languages.setModel(languageModel);
+
+        // Display new language section only when "New language..." selected
+        this.languages.addActionListener(e ->
+            this.newLanguagePanel.setVisible(this.languages.getSelectedIndex() == this.languages.getModel().getSize() - 1)
+        );
+        this.newLanguagePanel.setVisible(mpsLanguages.size() == 0);
+    }
+
+    public boolean isConfirmed() {
+        return this.confirmed;
+    }
+
+    public File[] getFiles() {
+        return this.files.toArray(new File[0]);
+    }
+
+    /**
+     * Called, when user presses the "Import" button.
+     */
+    private void cancelDialog() {
+        this.confirmed = false;
+        this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+    }
+
+    /**
+     * Called, when user presses the "Import" button.
+     */
+    private void confirmDialog() {
+        if (this.files.size() == 0) {
+            this.showError("No grammar files selected!");
+            return;
+        }
+
+        // "New language..." selected
+        if (this.languages.getSelectedIndex() == this.languages.getModel().getSize() - 1) {
+            if (this.languageName.getText().length() == 0) {
+                this.showError("Language name empty!");
+                return;
+            }
+            // TODO: Verify unique language name
+            // TODO: Verify valid language name
+        }
+
+        this.confirmed = true;
+        this.cancelDialog();
+    }
+
+    private void showError(String message) {
+        JOptionPane.showMessageDialog(this, message);
+    }
+
+    /**
+     * Prompts user through file dialog to choose grammar files and adds them into the file list and the files field.
+     */
+    private void addFiles() {
+        JFileChooser dialog = new JFileChooser();
+        dialog.setMultiSelectionEnabled(true);
+        dialog.addChoosableFileFilter(new FileNameExtensionFilter("ANTLRv4", "g4", "g"));
+        dialog.showOpenDialog(this);
+
+        // Add selected files
+        files.addAll(Arrays.asList(dialog.getSelectedFiles()));
+
+        // Remove duplicates from the files list
+        files = files
+            .stream()
+            .filter(distinctFile(File::getPath))
+            .collect(Collectors.toList());
+
+        this.syncFilesAndList();
+    }
+
+    /**
+     * Removes rows selected in the list from the file list and the files field.
+     */
+    private void removeFiles() {
+        this.fileList
+            .getSelectedValuesList()
+            .stream()
+            .forEach(i -> this.files.removeIf(f -> f.getName().equals(i)));
+
+        this.syncFilesAndList();
+    }
+
+    /**
+     * Synchronizes content of the files field and the files list.
+     */
+    private void syncFilesAndList() {
+        DefaultListModel listModel = (DefaultListModel) this.fileList.getModel();
+        listModel.clear();
+        files
+            .stream()
+            .map(File::getName)
+            .forEach(listModel::addElement);
+    }
+
+    /**
+     * Predicate used for detecting non-unique elements of an array.
+     *
+     * @param keyExtractor Object property extractor
+     * @param <T> Type of the object
+     * @return A Predicate that allows array unique operation.
+     */
+    private static <T> Predicate<T> distinctFile(Function<? super T,Object> keyExtractor) {
+        Map<Object,Boolean> seen = new ConcurrentHashMap<>();
+        return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+}
--- plugin/src/importer/src/premun/mps/ingrid/importer/Main.java
+++ plugin/src/importer/src/premun/mps/ingrid/importer/Main.java
@@ -1,11 +1,10 @@
 package premun.mps.ingrid.importer;
 
-import premun.mps.ingrid.parser.*;
-import premun.mps.ingrid.parser.grammar.*;
+import java.util.*;
 
 // TODO: Delete Main
 public class Main {
     public static void main(String[] args) {
-        GrammarInfo grammar = GrammarParser.parseFile("/home/premun/Diplomka/ingrid/grammars/SimpleXML.g4");
+        new ImportForm(new ArrayList<>());
     }
 }
